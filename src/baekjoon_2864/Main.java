49f390030c649234e6e2938d9726f690be596036
minwoo <sys_call@kakao.com>
2022-07-26 11:19:06 +0900

    Baekjoon_2864

---
+++ src/baekjoon_2864/Main.java
@@ -0,0 +1,22 @@
+package baekjoon_2864;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String[] str = sc.nextLine().split(" ");
+		
+		String N = str[0];
+		String M = str[1];
+		
+		int N_min, N_max, M_min, M_max;
+		
+		N_min = Integer.parseInt(N.replaceAll("6", "5"));
+		N_max = Integer.parseInt(N.replaceAll("5", "6"));
+		M_min = Integer.parseInt(M.replaceAll("6", "5"));
+		M_max = Integer.parseInt(M.replaceAll("5", "6"));
+		
+		System.out.printf("%d %d\n", N_min+M_min, N_max+M_max);
+	}
+}
