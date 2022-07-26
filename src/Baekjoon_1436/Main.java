d11add780179e2f2df6aa455c011041212080324
minwoo <sys_call@kakao.com>
2022-07-26 13:58:02 +0900

    Baekjoon_1436

---
+++ src/Baekjoon_1436/Main.java
@@ -0,0 +1,22 @@
+package Baekjoon_1436;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		int num = 666;
+		int cnt = 1;
+		
+		while(cnt < N) {
+			num++;
+			if(String.valueOf(num).contains("666")) {
+				cnt++;
+			}
+		}
+		System.out.println(num);
+	}
+}
