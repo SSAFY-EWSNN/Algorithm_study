ec34cad3b41c365361650fa5a76f6ad75c3406b8
seulgi <hanny1226@naver.com>
2022-08-03 19:27:54 +0900

    완전탐색 2주차

--- seulgi/src/bruteforce/Baekjoon_2231.java
+++ seulgi/src/bruteforce/Baekjoon_2231.java
@@ -12,12 +12,10 @@ public class Baekjoon_2231 {
         for(int i=1; i<=N; i++){
             int num = i;
             int sum = 0;
-
             while (num != 0){
                 sum += num % 10;
                 num /= 10;
             }
-
             if(sum + i == N){
                 res = i;
                 break;
