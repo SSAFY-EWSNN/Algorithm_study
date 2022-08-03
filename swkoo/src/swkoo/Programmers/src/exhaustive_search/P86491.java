960306f73638be9d6d619b2c7bcb777f4fcaee19
sungwookoo <sungwookoo.dev@gmail.com>
2022-08-03 23:04:49 +0900

    구성우 커밋

---
+++ swkoo/src/swkoo/Programmers/src/exhaustive_search/P86491.java
@@ -0,0 +1,42 @@
+package exhaustive_search;
+
+public class P86491 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] sizes = {{60, 50}, {30, 70}, {60, 30}, {80, 40}};
+		System.out.println(solution(sizes));
+		
+		
+		
+	}
+	
+	 static int solution(int[][] sizes) {
+	        int answer = 0;
+	        for(int i =0; i<sizes.length;i++) {
+	        	for(int j =0; j<1; j++) {
+	        		if(sizes[i][j] < sizes[i][j+1]) {
+	        			int temp = sizes[i][j];
+	        			sizes[i][j] = sizes[i][j+1];
+	        			sizes[i][j+1] = temp;
+	        		}
+	        	}
+	        }
+	        
+	        int maxW = 0;
+	        int maxH = 0;
+	        
+	        for(int i =0; i<sizes.length;i++) {
+	        	for(int j=0; j<2; j++) {
+	        		if(j==0) maxW = Math.max(maxW, sizes[i][j]);
+	        		else maxH = Math.max(maxH, sizes[i][j]);
+	        	}
+	        }
+	        
+	        answer = maxH * maxW;
+	        
+	        return answer;
+	    }
+	
+
+}
