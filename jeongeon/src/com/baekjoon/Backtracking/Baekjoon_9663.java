917ce4c4abc85af9256693dcc3c4469c17dcfe59
sje1942 <sje1942@naver.com>
2022-08-07 01:08:06 +0900

    baekjoon

---
+++ jeongeon/src/com/baekjoon/Backtracking/Baekjoon_9663.java
@@ -0,0 +1,20 @@
+package com.baekjoon.Backtracking;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon_9663 {
+	
+	public static void main(String[] args) throws IOException {
+		System.setIn(new FileInputStream("input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		// br.readLine();
+		// sb.append("추가할 내용");
+		// System.out.print(sb);
+		
+	}
+}
