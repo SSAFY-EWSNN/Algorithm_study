f7df0f52fc2856ccf30221f9555d0c5939054ee9
성정언 <sje1942@naver.com>
2022-08-15 00:09:09 +0900

    220814

--- jeongeon/src/com/baekjoon/BruteForce/Baekjoon_3085.java
+++ jeongeon/src/com/baekjoon/BruteForce/Baekjoon_3085.java
@@ -4,40 +4,30 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+
 public class Baekjoon_3085 {
+
 	public static void main(String[] args) throws Exception {
 
 		/**
 		 * 0. 입력파일 읽어들이기
 		 */
 		//System.setIn(new FileInputStream("input.txt"));
-		System.setIn(new FileInputStream("jeongeon/input.txt"));
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
 		// 결과를 한 번에 출력하기 위한 StringBuilder
 		//StringBuilder sb = new StringBuilder();
-		// StringTokenize 객체 선언
-		// StringTokenizer st = null;
-		
-		int n = Integer.parseInt(in.readLine());
 
 		// StringTokenize 객체 선언
 		// StringTokenizer st = null;
 
 		int n = Integer.parseInt(in.readLine());
+
 		//candy 배열 받기
 		String[][] candy = new String[n][n];
 		for(int i=0; i<n; i++) {
 			String[] a = in.readLine().split("");
 			for(int j=0; j<a.length; j++) {
-				candy[i][j] = a[j];	
-			}
-		}
-		
-		int count = 1, max = 0;
-	
-		
-=======
 				candy[i][j] = a[j];
 			}
 		}
@@ -45,7 +35,6 @@ public class Baekjoon_3085 {
 		int count = 1, max = 0;
 
 
->>>>>>> 463e3f6ed935b6dc19f36061033fb8c9b6c4e42e
 		for(int i=0; i<n; i++) {
 			for(int j=0; j<n-1; j++) {
 				swap(candy, i, j, i, j+1); //swap 행
@@ -66,11 +55,7 @@ public class Baekjoon_3085 {
 					count = 1;
 				}
 				swap(candy, i, j+1, i, j); //다시 제자리
-<<<<<<< HEAD
-				
-=======
 
->>>>>>> 463e3f6ed935b6dc19f36061033fb8c9b6c4e42e
 				swap(candy, j, i, j+1, i); //swap 열
 				for(int x=0; x<n; x++) {
 					for(int y=0; y<n-1; y++) {
@@ -91,18 +76,6 @@ public class Baekjoon_3085 {
 				swap(candy, j+1, i, j, i); //다시 제자리
 			}
 		}
-<<<<<<< HEAD
-		
-		System.out.println(max);
-	}
-	
-	 public static void swap(String[][] array, int i1, int i2, int j1, int j2) {
-        String temp = array[i1][i2];
-        array[i1][i2] = array[j1][j2];
-        array[j1][j2] = temp;        
-    }
-}
-=======
 
 		System.out.println(max);
 	}
@@ -113,4 +86,3 @@ public class Baekjoon_3085 {
 		array[j1][j2] = temp;
 	}
 }
->>>>>>> 463e3f6ed935b6dc19f36061033fb8c9b6c4e42e
