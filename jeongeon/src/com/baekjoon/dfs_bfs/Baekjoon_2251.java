f71f747c4cd27a162a2f3e4b6ac86a24dd1fd36a
sje1942 <sje1942@naver.com>
2022-08-16 22:54:07 +0900

    220816

--- jeongeon/src/com/baekjoon/dfs_bfs/Baekjoon_2251.java
+++ jeongeon/src/com/baekjoon/dfs_bfs/Baekjoon_2251.java
@@ -1,5 +1,23 @@
 package com.baekjoon.dfs_bfs;
+// baekjoon 2251 물통
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Baekjoon_2251 {
-
+	
+	public static void main(String[] args) throws IOException {
+		System.setIn(new FileInputStream("input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		String split[] = br.readLine().split(" ");
+		int A = Integer.parseInt(split[0]);
+		int B = Integer.parseInt(split[1]);
+		int C = Integer.parseInt(split[2]);
+		
+		
+	}
 }
